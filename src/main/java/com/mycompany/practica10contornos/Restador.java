2b53f7463368b7e3464a9bd87e4ccd89db7f5d61
ian <ianpancotera13@gmail.com>
2022-03-17 18:30:58 +0100

    tercera version - rama dev

---
+++ src/main/java/com/mycompany/practica10contornos/Restador.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.practica10contornos;
+
+
+public class Restador {
+public static float restar (float a, float b ){
+return a - b;
+}}
--- src/main/java/com/mycompany/practica10contornos/Sumador.java
+++
@@ -1,7 +0,0 @@
-
-package com.mycompany.practica10contornos;
-
-public class Sumador {
-public static float sumar (float a, float b ) {
-return a + b;
-}}
